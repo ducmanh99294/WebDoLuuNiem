e9d61795fef6f9162b76a714a1b98824ae41a7d7
manh <manh99294@donga.edu.vn>
2025-05-31 17:09:38 +0700

    crud product

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const connectMongoDB = require('./config/mongodbConfig.js');
+const productRoute = require('./routes/productRoute.js');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Xin chào từ Node.js + Express!');
 });
+
+connectMongoDB()
+
+app.use('/api/products', productRoute);
 
 app.listen(port, () => {
   console.log(`Server đang chạy tại http://localhost:${port}`);
