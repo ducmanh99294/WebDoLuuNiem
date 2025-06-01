fac0c147da0c1a6580ae1ec386e652da11275d7d
ducmanh <127844553+ducmanh99294@users.noreply.github.com>
2025-06-01 23:09:07 +0700

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,20 +7,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-import './App.css'
-import Header from './components/header'
-import Home from './components/pages/homepage'
-import Footer from './components/Fotter'
-
-function App() {
-  return (
-    <>
-      <Header />
-      <br></br>
-      <Home />
-      <Footer />
-    </>
-  )
-}
-
-export default App
