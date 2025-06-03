d99c073f96c84737b4ea2face9979ecfec92df4f
AnhBoHelloTeam <nhanhateku1@gmail.com>
2025-06-03 18:52:54 +0700

    thêm validateToken cho Chat + phân Role

--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -10,4 +10,12 @@ router.get('/:id', chatController.getChatById);
 router.put('/:id', chatController.updateChat);
 router.delete('/:id', chatController.deleteChat);
 
+// 
+// router.post('/', validateToken, chatController.createChat); // Yêu cầu token để tạo chat
+// router.get('/', validateToken, authRoles('admin'), chatController.getAllChats); // Chỉ admin được xem tất cả chat
+// router.get('/:id', validateToken, chatController.getChatById); // Yêu cầu token để xem chat theo ID
+// router.put('/:id', validateToken, authRoles('admin'), chatController.updateChat); // Chỉ admin được cập nhật chat
+// router.delete('/:id', validateToken, authRoles('admin'), chatController.deleteChat); // Chỉ admin được xóa chat
+//
+
 module.exports = router;
