27f6a478f24a3e333c8a5866b310074a30f82e3e
AnhBoHelloTeam <nhanhateku1@gmail.com>
2025-06-04 08:20:41 +0700

    kéo về

--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -12,13 +12,5 @@ router.get('/:id',validateToken, chatController.getChatById);
 router.put('/:id',validateToken, authRoles('admin'), chatController.updateChat);
 router.delete('/:id',validateToken, authRoles('admin'), chatController.deleteChat);
 
-// 
-// router.post('/', validateToken, chatController.createChat); // Yêu cầu token để tạo chat
-// router.get('/', validateToken, authRoles('admin'), chatController.getAllChats); // Chỉ admin được xem tất cả chat
-// router.get('/:id', validateToken, chatController.getChatById); // Yêu cầu token để xem chat theo ID
-// router.put('/:id', validateToken, authRoles('admin'), chatController.updateChat); // Chỉ admin được cập nhật chat
-// router.delete('/:id', validateToken, authRoles('admin'), chatController.deleteChat); // Chỉ admin được xóa chat
-//
-
 
 module.exports = router;
