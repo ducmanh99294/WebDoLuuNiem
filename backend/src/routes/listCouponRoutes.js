8c3b030f127c6bf8bdf909f07322918d78b60221
AnhBoHelloTeam <nhanhateku1@gmail.com>
2025-06-27 08:15:15 +0700

    De nhan2

--- backend/src/routes/listCouponRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const listCouponController = require('../controllers/listCouponController');
-const { validateToken } = require('../middlewares/authMiddleware');
-const authRoles = require('../middlewares/authRoles');
-
-router.post('/',  listCouponController.addCouponToUser);
-router.get('/',  listCouponController.getListCouponByUserId);
-router.delete('/:id', validateToken, listCouponController.removeCouponFromUser);
