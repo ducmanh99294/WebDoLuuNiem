52c441cecb17e3372e41868d23bd20d4ef3ce21e
chibanpco123 <hoang26022005sc@gmail.com>
2025-06-27 08:48:50 +0700

    update

--- frontend/src/components/pages/Dashboard.tsx
+++ frontend/src/components/pages/Dashboard.tsx
@@ -181,7 +181,7 @@ const Dashboard = () => {
 };
 
 const StatCard = ({ title, value }: { title: string; value: string }) => (
-  <div className="card">
+  <div className="card12">
     <div className="card-title">{title}</div>
     <div className="card-value">{value}</div>
   </div>
